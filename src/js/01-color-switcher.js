d67b448d32e11fe7ed7138754e6ffe203cac7680
Anastasia Les <anastasia@sonarmedia.com>
2021-12-26 10:18:21 +0200

    add first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
 
+const button = document.querySelectorAll('button');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let timerId = null;
+
+console.log(button);
+console.log(stopBtn);
+
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick)
+
+startBtn.style.padding = '10px 15px';
+stopBtn.style.padding = '10px 15px';
+
+// document.body.style.align = 'center';
+// stopBtn.style.marginRight = 'auto';
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onStartBtnClick(event) {
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    startBtn.disabled = true;
+    console.log(getRandomHexColor); 
+};
+
+function onStopBtnClick(event) {
+    clearInterval(timerId);
+    startBtn.disabled = false;
+    // console.log('It ends!');
+}
